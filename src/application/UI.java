b647aa757788e3615c5996b268be578b38a3164a
Vitor Ferreira <vrferreiragomes@gmail.com>
2026-02-24 15:00:19 -0300

    Implementing possible moves of Rook

--- src/application/UI.java
+++ src/application/UI.java
@@ -58,14 +58,14 @@ public class UI {
 	}
 
 	private static void printPiece(ChessPiece piece) {
-		if (piece == null) {
-			System.out.print("-");
-		} else {
-			if (piece.getColor() == Color.WHITE) {
-				System.out.print(ANSI_WHITE + piece + ANSI_RESET);
-			} else {
-				System.out.print(ANSI_YELLOW + piece + ANSI_RESET);
-			}
-		}
+	    if (piece == null) {
+	        System.out.print("- ");
+	    } else {
+	        if (piece.getColor() == Color.WHITE) {
+	            System.out.print(ANSI_WHITE + piece + ANSI_RESET + " ");
+	        } else {
+	            System.out.print(ANSI_YELLOW + piece + ANSI_RESET + " ");
+	        }
+	    }
 	}
-}
+	}
